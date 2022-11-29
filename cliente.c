b92fbd5d917f1ad79bfe55ca3c17e8a822173bd2
Guizzin14 <119328568+Guizzin14@users.noreply.github.com>
2022-11-29 13:31:41 -0300

    Update cliente.c

--- cliente.c
+++ cliente.c
@@ -1,70 +1,53 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <errno.h>
-#include <math.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <netdb.h>
+sock = socket(AF_INET , SOCK_STREAM , 0);
+    if (sock == -1)
+    {
+        printf("Could not create socket");
+    }
+    puts("Socket created");
 
-#define SERVER_IP "193.136.93.147"	
-#define SERVERPORT "4950" // porto
-#define MAXBUFLEN 100
+    server.sin_addr.s_addr = inet_addr("127.0.0.1");
+    server.sin_family = AF_INET;
+    server.sin_port = htons( 8888 );
 
-int main(int argc, char *argv[])
-{
-int num =0;
-int sockfd;
-struct addrinfo hints, *servinfo, *p;
-int rv;
-int numbytes;
-char mensagem[MAXBUFLEN];
-char temp [33];
+    //Connect to remote server
+    if (connect(sock , (struct sockaddr *)&server , sizeof(server)) < 0)
+    {
+        perror("connect failed. Error");
+        return 1;
+    }
 
-memset(&hints, 0, sizeof hints);
-hints.ai_family = AF_UNSPEC;
-hints.ai_socktype = SOCK_DGRAM;
+    puts("Connected\n");
 
-if ((rv = getaddrinfo(SERVER_IP, SERVERPORT, &hints, &servinfo)) != 0) {
- fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
- return 1;
+    //keep communicating with server
+    while(1)
+    {
+        printf("Enter message : ");
+    gets(message);
+    fflush(stdin);
+        //scanf("%s" , message);
+
+        //Send some data
+        if( send(sock , message, strlen(message) , 0) < 0)
+        {
+            puts("Send failed");
+            return 1;
+        }
+
+        //Receive a reply from the server
+        if( recv(sock , server_reply , 2000 , 0) < 0)
+        {
+            puts("recv failed");
+            break;
+        }
+
+        puts("Server reply :");
+        //puts(server_reply);
+    int vl = 0;
+    for(int i =0; server_reply[i] != '\0';i++)
+    {
+        if(server_reply[i] != ' ')
+            vl += (int)server_reply[i];
+    }
+        printf("%d\n",vl);
 }
-
-// pesquisa os resultados e cria a socket
-for(p = servinfo; p != NULL; p = p->ai_next) {
- if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
-	 perror("talker: socket");
-	 continue;
- }
-
- break;
- }
-
-if (p == NULL) {
- fprintf(stderr, "clinete: failed to bind socket\n");
- return 2;
-}
-
-for(num=0; num<100; num++) {
-temp[0]='\0';
-mensagem[0]='\0';
-strncpy(mensagem, "mensgem num x ", 14);
-snprintf(temp, 33, "%d", num);
-strncpy(mensagem+14, temp,33);
-//mensagem[46]='\0';
-printf("%s \n", mensagem);
-
-if ((numbytes = sendto(sockfd, mensagem, strlen(mensagem), 0, p->ai_addr, p->ai_addrlen)) == -1) {
-perror("cliente: a enviar");
-exit(1);
-}
-
-//freeaddrinfo(servinfo);
-}
-close(sockfd);
-
-return 0;
-}
+    close(sock);
