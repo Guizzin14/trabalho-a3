a7f814bba085c9f66c837bd872b6c8abc94d08e7
Guizzin14 <119328568+Guizzin14@users.noreply.github.com>
2022-11-29 02:33:44 -0300

    Update cliente.c

--- cliente.c
+++ cliente.c
@@ -1,119 +1,63 @@
-/*********************************
-Autor: Fernando Krein Pinheiro
-Data: 04/07/2012
-Linguagem: C
-========= IMPORTANTE ===========
-O código esta livre para usar,
-citar e compartilhar desde que
-mantida sua fonte e seu autor.
-Obrigado.
-*********************************
-*/
+// Cliente socket
+
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
-#include <sys/types.h>
+#include <string.h>
+#include <unistd.h>
 #include <sys/socket.h>
-#include <netdb.h>
+#include <sys/types.h>
 #include <netinet/in.h>
-#include <arpa/inet.h>
-#include <sys/ioctl.h>
-#include <pthread.h>
- 
-void* Servidor(void* arg)
-{
-    /*Buffer de entrada (armazena buffer do cliente)*/
-    char buffer_do_cliente[256];
-    /*Cast do ponteiro*/
-    int sockEntrada = *(int *) arg;
-    /*Loop "infinito"*/
-    printf("Aguardando as mensagens... ");
-    for (;;)
-    {
-        /*Le o que vem do cliente*/
-        read(sockEntrada, buffer_do_cliente, sizeof (buffer_do_cliente));
-        if (strcmp(buffer_do_cliente, "sair") != 0)
-        {
-            /*Se buffer == sair cai fora*/
-            printf("%s\n",buffer_do_cliente);
-        }
-        else
-             {
-                 /*Encerra o descritor*/
-                 close(sockEntrada);
-                 /*Encerra a thread*/
-                 pthread_exit((void*) 0);
-             }
-    }
+#include <netdb.h> // define a estrutura hostent, que será usada abaixo.
+
+void error(const char *msg){
+  perror(msg);
+  exit(0);
 }
- 
-int configuracaoServidor()
-{
-    /*Cria o descritor*/
-    int sockfd;
-    /*Declaracao da estrutura*/
-    struct sockaddr_in serverAddr;
-    /*Cria o socket*/
-    if ((sockfd = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
-    {
-      printf("Erro no Socket\n");
-      exit(1);
-    }
-    /*Zera a estrutura*/
-    memset(&serverAddr, 0, sizeof (serverAddr));
-    /*Seta a familia*/
-    serverAddr.sin_family = AF_INET;
-    /*Seta os IPS (A constante INADDR_ANY e todos os ips ou qualquer ip) htonl -> conversao*/
-    serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    /*Define a porta*/
-    serverAddr.sin_port = htons(6881);
-    /*Faz a bindagem (cola, gruda, conecta seja o que for)*/
-    if (bind(sockfd, (struct sockaddr *) & serverAddr, sizeof (serverAddr)) < 0)
-    {
-      printf("Erro no Socket\n");
-      exit(1);
-     }
-    /*Fica na escuta de ate 5 clientes*/
-    if (listen(sockfd, 5) < 0)
-    {
-      printf("Erro no Socket\n");
-      exit(1);
-    }
-    return sockfd;
-}
- 
-int main()
-{
-    system("clear");
-    /*Declaracao da estrutura*/
-    struct sockaddr_in serverAddr;
-    /*Retorna da funcao e o descritor*/
-    int sockfd = configuracaoServidor();
- 
-    /*Loop "infinito"*/
-    while (1)
-    {
-        int clienteSockfd;
-        struct sockaddr_in clienteAddr;
-        /*tamanho da estrutura*/
-        unsigned int clntLen;
-        clntLen = sizeof (clienteAddr);
-        /*declara uma thread*/
-    pthread_t thread;
-    /*Fica no aguardo da conexao do cliente*/
-        if ((clienteSockfd = accept(sockfd, (struct sockaddr *) & clienteAddr, &clntLen)) < 0)
-        {
-      printf("Erro no Socket\n");
-      exit(1);
-    }
-        /*Inicializa a thread*/
-        if (pthread_create(&thread, NULL, Servidor, &clienteSockfd) != 0)
-       {
-            printf("Erro na Thread\n");
-            exit(1);
-       }
- 
-        pthread_detach(thread);
-    }
+int main(int argc, char const **argv){
+
+  int servidor, porta, n;
+  struct sockaddr_in serv_addr;
+  struct hostent *server;
+
+  char buffer[256];
+  if(argc < 3){
+    printf("Entre com o servidor e a porta: ex: 127.0.0.1 3333 \n");
     exit(0);
+  }
+  porta = atoi(argv[2]);
+  servidor = socket(AF_INET, SOCK_STREAM, 0);
+  if(servidor < 0)
+    error("Erro no servidor socker");
+  server = gethostbyname(argv[1]);
+  if(server == NULL)
+    fprintf(stderr,"ERROR, no such host\n");
+  bzero((char *) &serv_addr,sizeof(serv_addr));
+
+  serv_addr.sin_family  = AF_INET;
+  bcopy((char *)server->h_addr,(char *)&serv_addr.sin_addr.s_addr, server->h_length);
+
+  serv_addr.sin_port = htons(porta);
+
+  // Inicio da conexão
+  if(connect(servidor,(struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    error("Erro ao conectar");
+
+  printf("Welcome to server\n");
+
+  printf("Por favor, entre com a Mensagem: ");
+
+  bzero(buffer, 256);
+  fgets(buffer,255,stdin);
+  n = write(servidor, buffer, strlen(buffer));
+  if(n < 0)
+    error("Erro ao escrever no sock");
+  bzero(buffer, 256);
+  n = read(servidor, buffer, 255);
+  if(n < 0)
+    error("Erro as ler o sock");
+  printf("%s\n",buffer);
+
+  close(servidor);
+
+  return 0;
 }
