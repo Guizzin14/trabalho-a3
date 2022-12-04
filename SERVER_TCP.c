ab583a712a886ffcf7a49a50ffd19b333ae80cbd
Guizzin14 <119328568+Guizzin14@users.noreply.github.com>
2022-12-04 16:36:15 -0300

    Update SERVER_TCP.c

--- SERVER_TCP.c
+++ SERVER_TCP.c
@@ -1,263 +1,114 @@
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <arpa/inet.h>
-#include <netinet/in.h>
+/*
+NOTES :
+Stages for server
+    Socket creation:
+    int sockfd = socket(domain, type, protocol)
+    sockfd: socket descriptor, an integer (like a file-handle)
+    domain: integer, communication domain e.g., AF_INET (IPv4 protocol) , AF_INET6 (IPv6 protocol)
+    type: communication type
+    SOCK_STREAM: TCP(reliable, connection oriented)
+    SOCK_DGRAM: UDP(unreliable, connectionless)
+    protocol: Protocol value for Internet Protocol(IP), which is 0. This is the same number which appears on protocol field in the IP header of a packet.(man protocols for more details)
+    Setsockopt:
+    int setsockopt(int sockfd, int level, int optname,  
+                       const void *optval, socklen_t optlen);
+    This helps in manipulating options for the socket referred by the file descriptor sockfd. This is completely optional, but it helps in reuse of address and port. Prevents error such as: “address already in use”.
+    Bind:
+    int bind(int sockfd, const struct sockaddr *addr, 
+                              socklen_t addrlen);
+    After creation of the socket, bind function binds the socket to the address and port number specified in addr(custom data structure). In the example code, we bind the server to the localhost, hence we use INADDR_ANY to specify the IP address.
+    Listen:
+    int listen(int sockfd, int backlog);
+    It puts the server socket in a passive mode, where it waits for the client to approach the server to make a connection. The backlog, defines the maximum length to which the queue of pending connections for sockfd may grow. If a connection request arrives when the queue is full, the client may receive an error with an indication of ECONNREFUSED.
+    Accept:
+    int new_socket= accept(int sockfd, struct sockaddr *addr, socklen_t *addrlen);
+    It extracts the first connection request on the queue of pending connections for the listening socket, sockfd, creates a new connected socket, and returns a new file descriptor referring to that socket. At this point, connection is established between client and server, and they are ready to transfer data.
+struct sockaddr {
+        ushort  sa_family;
+        char    sa_data[14];
+};
+struct sockaddr_in{  
+    short sin_family;  
+    unsigned short sin_port;  
+struct in_addr sin_addr;  
+    char sin_zero[8];  
+};  
+Parameters :
+sin_family
+Address family (must be AF_INET).
+sin_port
+IP port.
+sin_addr
+IP address.
+sin_zero
+Padding to make structure the same size as SOCKADDR.
+This is the form of the SOCKADDR structure specific to the Internet address family and can be cast to SOCKADDR.
+The IP address component of this structure is of type IN_ADDR. The IN_ADDR structure is defined in Windows Sockets header file WINSOCK.H
+*/
 #include <stdio.h>
 #include <unistd.h>
-#include <string.h>
+#include <netinet/in.h>
 #include <stdlib.h>
-#include <dirent.h>
+#include <string.h>
 #include <sys/types.h>
-#include <sys/stat.h>
-#include <unistd.h>
-#include <errno.h>
-#include <fcntl.h>
-#include <sys/sendfile.h>
-#include <ctype.h>
-#include <pthread.h>
+#include <sys/socket.h>
 
-void *connection_handler(void *);
+void error(char* msg){
+		perror(msg);
+		exit(1);
+}
 
-#define MAX_MSG 1024
-
-/*
- Servidor aguarda por mensagem do cliente, imprime na tela
- e depois envia resposta e finaliza processo
- */
-
-int main(int argc, char* argv[]) {
-
-    //Variaveis auxiliares para encontrar o arquivo a ser transferido.
-    DIR *mydir;
-    struct dirent *myfile;
-    struct stat mystat;
-    //verificando se foi executando o comando corretamente
-    if (argc != 3) {
-        fprintf(stderr, "use:./server [Porta] [local]\n");
-        return -1;
-    } else if (!isdigit(*argv[1])) {
-        fprintf(stderr, "Argumento invalido '%s'\n", argv[1]);
-        fprintf(stderr, "use:./server [Porta] [local]\n");
-        return -1;
-    }
-
-    mydir = opendir(argv[2]);
-    //verificando se o diretorio existe
-    if(mydir == NULL ){fprintf(stderr, "Argumento invalido '%s'\n", argv[2]);return -1;}
-    char* aux1 = argv[1];
-        int portaServidor = atoi(aux1);
-
-   //variaveis
-    int socket_desc, conexao, c, nova_conex;
-    struct sockaddr_in servidor, cliente;
-    char *mensagem;
-    char resposta[MAX_MSG];
-    int tamanho, count;
-
-    // para pegar o IP e porta do cliente  
-    char *cliente_ip;
-    int cliente_port;
-
-    //*********************************************************************//
-    //      INICIO DO TRATAMENTO DA THREAD, localizaÃ§Ã£o e transferencia    // 
-    //      do arquivo.                                                    // 
-    //*********************************************************************//
-    void *connection_handler(void *socket_desc) {
-        /*********************************************************/
-
-        /*********comunicao entre cliente/servidor****************/
-
-        // pegando IP e porta do cliente
-        cliente_ip = inet_ntoa(cliente.sin_addr);
-        cliente_port = ntohs(cliente.sin_port);
-        printf("cliente conectou: %s : [ %d ]\n", cliente_ip, cliente_port);
-
-        // lendo dados enviados pelo cliente
-        //mensagem 1 recebido nome do arquivo   
-        if ((tamanho = read(conexao, resposta, MAX_MSG)) < 0) {
-            perror("Erro ao receber dados do cliente: ");
-            return NULL;
-        }
-        resposta[tamanho] = '\0';
-        printf("O cliente falou: %s\n", resposta);
-
-        char aux_nomeArquivo[MAX_MSG];
-        //fazendo cÃ³pia do nome do arquivo para variÃ¡vel auxiliar. tal variavel Ã© utilzada para localizar
-        // o arquivo no diretorio.
-        strncpy(aux_nomeArquivo, resposta, MAX_MSG);
-        //printf("ax_nomeArquivo: %s\n", aux_nomeArquivo);
-
-
-
-        /*********************************************************/
-        if (mydir != NULL) {
-
-            //funÃ§Ã£o busca todo o diretÃ³rio buscando o arquivo na variavel aux_nomeArquivo
-            //struct stat s;
-            while ((myfile = readdir(mydir)) != NULL) {
-
-                stat(myfile->d_name, &mystat);
-
-                printf("Arquivo lido: %s, Arquivo procurado: %s\n", myfile->d_name, resposta);
-                if (strcmp(myfile->d_name, resposta) == 0) {//arquivo existe
-                   closedir(mydir);
-                    //Reiniciando variÃ¡veis da pesquisa do diretorio para a proxima thread
-                    myfile = NULL;
-                    mydir = NULL;
-                    mydir = opendir(argv[2]);
-
-                    //**************************************//
-                    //      INICIO DO PROTOCOLO            //
-                    //*************************************//
-
-
-                    mensagem = "200";
-                    //mensagem 2 - enviando confirmaÃ§Ã£o q arquivo existe
-                    write(conexao, mensagem, strlen(mensagem));
-
-                    //mensagem 3 - recebendo que arquivo OK do cliente
-                    read(conexao, resposta, MAX_MSG);
-
-
-                    //**************************************//
-                    //      FIM DO PROTOCOLO               //
-                    //*************************************//
-
-                    //abrindo o arquivo e retirando o tamanho//
-                    //fazendo cÃ³pia do nome do arquivo para variÃ¡vel auxiliar. tal variavel Ã© utilzada para localizar
-                    // o arquivo no diretorio.
-
-
-                    char localArquivo[1024]; 
-                    strncpy(localArquivo, argv[2], 1024);
-                    strcat(localArquivo,aux_nomeArquivo);
-
-                    FILE * f = fopen(localArquivo, "rb");
-                    if((fseek(f, 0, SEEK_END))<0){printf("ERRO DURANTE fseek");}
-                    int len = (int) ftell(f);                   
-                    mensagem = (char*) len;
-                    printf("Tamanho do arquivo: %d\n", len);
-                    //convertendo o valor do tamanho do arquivo (int) para ser enviado em uma mensagem no scoket(char)
-                    char *p, text[32];
-                    int a = len;
-                    sprintf(text, "%d", len);
-                    mensagem = text;
-
-                    //mensagem 4 - enviando o tamanho do arquivo
-                    send(conexao, mensagem, strlen(mensagem), 0);
-
-                    int fd = open(localArquivo, O_RDONLY);
-                    off_t offset = 0;
-                    int sent_bytes = 0;
-                    //localArquivo = NULL;
-                    if (fd == -1) {
-                        fprintf(stderr, "Error opening file --> %s", strerror(errno));
-
-                        exit(EXIT_FAILURE);
-                    }
-
-                    while (((sent_bytes = sendfile(conexao, fd, &offset, BUFSIZ)) > 0)&& (len > 0)) {
-
-                        fprintf(stdout, "1. Servidor enviou %d bytes do arquivo, offset Ã© agora : %d e os dados restantes = %d\n", sent_bytes, (int)offset, len);
-                        len -= sent_bytes;
-                        fprintf(stdout, "2.Servidor enviou %d bytes do arquivo, offset Ã© agora : %d e os dados restantes = %d\n", sent_bytes, (int)offset, len);
-                        if (len <= 0) {
-                            break;
-                        }
-                    }
-                    //closedir(mydir);
-                    while (1) {
-                    }
-
-                }
-            }if(myfile==NULL) {
-                    //enviando mensagem para o cliente de arquivo nao encontrado.
-                    mensagem = "404";//file not found
-                    printf("\n//*********************************//\n");
-                    printf("Arquivo \"%s\" NÃ£o Existe no diretÃ³rio: \"%s\"\n",aux_nomeArquivo, argv[2]);
-                    //mensagem 2 - enviando confirmaÃ§Ã£o q arquivo existe
-                    write(conexao, mensagem, strlen(mensagem));
-                    //sempre que termina de pesquisar o diretorio de arquivos a variavel myfile vai para null
-                    // entao eh necessario preencher mydir novamente com o argv[2] com o diretorio de pesquisa. 
-                    //caso contrario novas thread nao acessaram o diretorio passado em argv[2]]
-                    mydir = opendir(argv[2]);
-                    //
-                    while (1) {
-                    }
-                    close(conexao);
-                    //closedir(mydir);
-
-            }
-            if (mydir != NULL) {
-                closedir(mydir);
-                mydir = NULL;
-            }
-        }
-
-        if (strcmp(resposta, "bye\n") == 0) {
-            close(conexao);
-            printf("Servidor finalizado...\n");
-            return NULL;
-        }
-
-
-    }
-
-    //*********************************************************************//
-    //      FIM DO TRATAMENTO DA THREAD, localizaÃ§Ã£o e transferencia    // 
-    //      do arquivo.                                                    // 
-    //*********************************************************************//
-
-
-
-    //************************************************************
-    /*********************************************************/
-    //Criando um socket
-    socket_desc = socket(AF_INET, SOCK_STREAM, 0);
-    if (socket_desc == -1) {
-        printf("Nao foi possivel criar o socket\n");
-        return -1;
-    }
-
-    //Preparando a struct do socket
-    servidor.sin_family = AF_INET;
-    servidor.sin_addr.s_addr = INADDR_ANY; // Obtem IP do S.O.
-    servidor.sin_port = htons(portaServidor);
-
-    //Associando o socket a porta e endereco
-    if (bind(socket_desc, (struct sockaddr *) &servidor, sizeof (servidor)) < 0) {
-        puts("Erro ao fazer bind Tente outra porta\n");
-        return -1;
-    }
-    puts("Bind efetuado com sucesso\n");
-
-    // Ouvindo por conexoes
-    listen(socket_desc, 3);
-    /*********************************************************/
-
-    //Aceitando e tratando conexoes
-
-    puts("Aguardando por conexoes...");
-    c = sizeof (struct sockaddr_in);
-
-    while ((conexao = accept(socket_desc, (struct sockaddr *) &cliente, (socklen_t*) & c))) {
-        if (conexao < 0) {
-            perror("Erro ao receber conexao\n");
-            return -1;
-        }
-
-        pthread_t sniffer_thread;
-        nova_conex = (int) malloc(1);
-        nova_conex = conexao;
-
-        if (pthread_create(&sniffer_thread, NULL, connection_handler, (void*) nova_conex) < 0) {
-            perror("could not create thread");
-            return 1;
-        }
-        puts("Handler assigned");
-    }
-    if (nova_conex < 0) {
-        perror("accept failed");
-        return 1;
-    }
+int main(int argc, char *argv[]){
+	if(argc<2){
+			fprintf(stderr,"No port provided\n");
+			exit(1);
+	}	
+	int sockfd, newsockfd, portno, n;	//File descriptor for Sockets
+	char buffer[255];	//Temporary buffer to read and write data
+	
+	struct sockaddr_in serv_addr,cli_addr;	//Structure to hold Server and Client details
+	socklen_t cli_len;	//Client length 
+	
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);	//AF_INET refers to addresses from the internet, IP addresses specifically. SOCK_STREAM to refer TCP(SOCK_DGRAM for UDP), 0 again for TCP
+	if(sockfd<0){
+			error("Socket failed \n");
+	}
+	bzero((char*) &serv_addr,sizeof(serv_addr));	//The bzero() function shall place n zero-valued bytes in the area pointed to by s.
+	portno = atoi(argv[1]);
+	
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = INADDR_ANY;	//INADDR_ANY is used when you don't need to bind a socket to a specific IP. When you use this value as the address when calling bind() , the socket accepts connections to all the IPs of the machine.
+	serv_addr.sin_port = htons(portno);	//the htons() function converts values between host and network byte orders. There is a difference between big-endian and little-endian and network byte order depending on your machine and network protocol in use.
+	
+	if(bind(sockfd, (struct sockaddr*) &serv_addr,sizeof(serv_addr))<0){	//Binding the socket
+		error("Binding failed\n");
+	}
+	
+	listen(sockfd, 5);//Listening for new Connections
+	cli_len = sizeof(cli_addr);
+	
+	newsockfd = accept(sockfd, (struct sockaddr*) &cli_addr, &cli_len);//Accept the new connection
+	
+	if(newsockfd<0)
+		error("error accepting new connection \n");
+	
+	while(1){
+		bzero(buffer, 256);
+		n = read(newsockfd, buffer, 255);	//Read incoming data streams
+		if(n<0)
+			error("Error reading from Client");
+		printf("Client : %s \n", buffer);
+		bzero(buffer, 255);
+		printf("Server :");
+		fgets(buffer, 255, stdin);
+		n = write(newsockfd, buffer, strlen(buffer));
+		if(n<0)
+			error("Error writing to server\n");
+		if(strncmp(buffer, "Bye", 3) == 0)
+			break;
+	}
+	
+	close(sockfd);
+	close(newsockfd);
+	return 0;
 }
