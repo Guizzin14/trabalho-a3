dd57576c930f62be69e391138353021d0a5c9d38
Guizzin14 <119328568+Guizzin14@users.noreply.github.com>
2022-12-04 16:56:48 -0300

    Update CLIENTE_TCP.c

--- CLIENTE_TCP.c
+++ CLIENTE_TCP.c
@@ -1,69 +1,61 @@
-/*
- * Title : Full duplex client side 
- * Name : Aditya Pratap Singh Rajput
- * Subject : Network Protocols And Programming using C
-Note : please consider the TYPOS in comments.
-Thanks.
-*/
-
-#include "stdio.h"
-#include "stdlib.h"
-#include "string.h"
-//headers for socket and related functions
-#include <sys/types.h>
+#include <stdio.h>
+#include <string.h>
 #include <sys/socket.h>
-//for including structures which will store information needed
-#include <netinet/in.h>
-#include <unistd.h>
-//for gethostbyname
-#include "netdb.h"
-#include "arpa/inet.h"
+#include <arpa/inet.h>
 
-int main()
+int main(void)
 {
-int socketDescriptor;
-
-struct sockaddr_in serverAddress;
-char sendBuffer[1000],recvBuffer[1000];
-
-pid_t cpid;
-
-bzero(&serverAddress,sizeof(serverAddress));
-
-serverAddress.sin_family=AF_INET;
-serverAddress.sin_addr.s_addr=inet_addr("127.0.0.1");
-serverAddress.sin_port=htons(5500);
-
-/*Creating a socket, assigning IP address and port number for that socket*/
-socketDescriptor=socket(AF_INET,SOCK_STREAM,0);
-
-/*Connect establishes connection with the server using server IP address*/
-connect(socketDescriptor,(struct sockaddr*)&serverAddress,sizeof(serverAddress));
-
-/*Fork is used to create a new process*/
-cpid=fork();
-if(cpid==0)
-{
-while(1)
-{
-bzero(&sendBuffer,sizeof(sendBuffer));
-printf("\nType a message here ...  ");
-/*This function is used to read from server*/
-fgets(sendBuffer,10000,stdin);
-/*Send the message to server*/
-send(socketDescriptor,sendBuffer,strlen(sendBuffer)+1,0);
-printf("\nMessage sent !\n");
+    int socket_desc;
+    struct sockaddr_in server_addr;
+    char server_message[2000], client_message[2000];
+    
+    // Clean buffers:
+    memset(server_message,'\0',sizeof(server_message));
+    memset(client_message,'\0',sizeof(client_message));
+    
+    // Create socket:
+    socket_desc = socket(AF_INET, SOCK_STREAM, 0);
+    
+    if(socket_desc < 0){
+        printf("Unable to create socket\n");
+        return -1;
+    }
+    
+    printf("Socket created successfully\n");
+    
+    // Set port and IP the same as server-side:
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(2000);
+    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    
+    // Send connection request to server:
+    if(connect(socket_desc, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0){
+        printf("Unable to connect\n");
+        return -1;
+    }
+    printf("Connected with server successfully\n");
+    
+    // Get input from the user:
+    printf("Enter message: ");
+    gets(client_message);
+    
+    // Send the message to server:
+    if(send(socket_desc, client_message, strlen(client_message), 0) < 0){
+        printf("Unable to send message\n");
+        return -1;
+    }
+    
+    // Receive the server's response:
+    if(recv(socket_desc, server_message, sizeof(server_message), 0) < 0){
+        printf("Error while receiving server's msg\n");
+        return -1;
+    }
+    
+    printf("Server's response: %s\n",server_message);
+    
+    // Close the socket:
+    close(socket_desc);
+    
+    return 0;
 }
-}
-else
-{
-while(1)
-{
-bzero(&recvBuffer,sizeof(recvBuffer));
-/*Receive the message from server*/
-recv(socketDescriptor,recvBuffer,sizeof(recvBuffer),0);
-printf("\nSERVER : %s\n",recvBuffer);
-}
-}
-return 0;
-}
+
