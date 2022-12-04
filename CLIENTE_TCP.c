160423a146b6e57c8565de9bd0bd644c9342de68
Guizzin14 <119328568+Guizzin14@users.noreply.github.com>
2022-12-04 16:37:36 -0300

    Update CLIENTE_TCP.c

--- CLIENTE_TCP.c
+++ CLIENTE_TCP.c
@@ -1,127 +1,93 @@
 
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <arpa/inet.h>
-#include <netinet/in.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <string.h>
-#include <stdlib.h>
-
-#define MAX_MSG 1024
-
 /*
- Cliente envia mensagem ao servidor e imprime resposta
- recebida do Servidor
+ * =====================================================================================
+ *
+ *       Filename:  client.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2013年08月19日 17时19分33秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
  */
 
-int main(int argc, char *argv[]) {
-    FILE *received_file;
-    received_file = fopen(argv[3], "w");
-    ssize_t len;
-    char buffer[BUFSIZ];
-    int aba;
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <error.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
 
-      if (argc != 4) {
-        fprintf(stderr, "use:./cliente [IP] [Porta] [arquivo]\n");
-        return -1;
-    } else if (!isdigit(*argv[2])) {
-        fprintf(stderr, "Argumento invalido '%s'\n", argv[2]);
-        fprintf(stderr, "use:./cliente [IP] [Porta] [arquivo]\n");
-        return -1;
-    }
+#define SEVPORT 3333
+#define MAXDATASIZE  (1024*5)
 
-    // variaveis
-    int socket_desc;
-    struct sockaddr_in servidor;
-    char *mensagem;
-    char resposta_servidor[MAX_MSG];
-    int tamanho;
-    mensagem = argv[3];
-    char* aux1 = argv[2];
-    int portaServidor = atoi(aux1);
+#define TIME_DIFF(t1,t2) (((t1).tv_sec-(t2).tv_sec)*1000+((t1).tv_usec-(t2).tv_usec)/1000)
 
-    /*****************************************/
-    /* Criando um socket */
-    // AF_INET = ARPA INTERNET PROTOCOLS
-    // SOCK_STREAM = orientado a conexao
-    // 0 = protocolo padrao para o tipo escolhido -- TCP
-    socket_desc = socket(AF_INET, SOCK_STREAM, 0);
+main(int argc, char *argv[])
+{
+	int sockfd,sendbytes,recvbytes;
+	char buf[MAXDATASIZE];
+	struct hostent *host;
+	struct sockaddr_in serv_addr;
+	struct timeval timestamp;
+	struct timeval timestamp_end;
+	if(argc < 2){
+		fprintf(stderr,"Please enter the server's hostname!\n");
+		exit(1);
+	}
 
-    if (socket_desc == -1) {
-        printf("Nao foi possivel criar socket\n");
-        return -1;
-    }
+	if((host=gethostbyname(argv[1])) == NULL){
+		perror("gethostbyname:");
+		exit(1);
+	}
+#ifdef CLIENT_DEBUG
+	printf("hostent h_name: %s , h_aliases: %s,\
+			h_addrtype: %d, h_length: %d, h_addr_list: %s\n",\
+			host->h_name,*(host->h_aliases),host->h_addrtype,host->h_length,*(host->h_addr_list));
+#endif
 
-    /* Informacoes para conectar no servidor */
-    // IP do servidor
-    // familia ARPANET
-    // Porta - hton = host to network short (2bytes)
-    servidor.sin_addr.s_addr = inet_addr(argv[1]);
-    servidor.sin_family = AF_INET;
-    servidor.sin_port = htons(portaServidor);
+	if((sockfd = socket(AF_INET,SOCK_STREAM,0)) == -1){
+		perror("socket:");
+		exit(1);
+	}
 
-    //Conectando no servidor remoto
-    if (connect(socket_desc, (struct sockaddr *) &servidor, sizeof (servidor)) < 0) {
-        printf("Nao foi possivel conectar\n");
-        return -1;
-    }
-    printf("Conectado no servidor\n");
-    /*****************************************/
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = htons(SEVPORT);
+	serv_addr.sin_addr = *((struct in_addr *)host->h_addr);
+	bzero(&(serv_addr.sin_zero),8);
 
+	if(connect(sockfd,(struct sockaddr *)&serv_addr, \
+					sizeof(struct sockaddr)) == -1){
+		perror("connect:");
+		exit(1);
+	}
+	memset(buf,0x15, sizeof(buf));
+	gettimeofday(&timestamp,NULL);
+		
+	if((sendbytes = send(sockfd,buf,sizeof(buf),0)) == -1){
+		perror("send:");
+		exit(1);
+	}
 
-    /*******COMUNICAO - TROCA DE MENSAGENS **************/
+	gettimeofday(&timestamp_end, NULL);
+	printf("sendbytes: %d,time_stamp: %ld ms\n",sendbytes,TIME_DIFF(timestamp_end,timestamp));
 
-    //Enviando uma mensagem
-    //mensagem 1 enviando nome do arquivo.  
-    if (send(socket_desc, mensagem, strlen(mensagem), 0) < 0) {
-        printf("Erro ao enviar mensagem\n");
-        return -1;
-    }
-    printf("Dados enviados\n");
-
-    memset(mensagem, 0, sizeof mensagem);
-    memset(resposta_servidor, 0, sizeof resposta_servidor);
-
-    //Recebendo resposta do servidor
-    //mensagem 2 recebendo que arquivo existe   
-    if((tamanho = read(socket_desc, resposta_servidor, MAX_MSG)) < 0) {
-        printf("Falha ao receber resposta\n");
-        return -1;
-    }
-
-    printf("Resposta recebida: %s\n", resposta_servidor);
-    if (strcmp(resposta_servidor, "200") == 0) {
-
-        mensagem = "OK";
-        //mensagem 3 enviado ok
-        write(socket_desc, mensagem, strlen(mensagem));
-        //mensagem 4 recebendo o tamanho do arquivo;
-        memset(resposta_servidor, 0, sizeof resposta_servidor);
-        read(socket_desc, resposta_servidor, 1024);
-
-        int tamanhoDoArquivo = atoi(resposta_servidor);
-        printf("\nTamanho do arquivo a ser copiado: %s \n", resposta_servidor);
-        aba = tamanhoDoArquivo;
-
-    }else{
-        fprintf(stderr, "Arquivo nao encontrado no servirdor'%s'\n", argv[3]);
-        close(socket_desc);
-        printf("Cliente finalizado com sucesso!\n");
-        return 0;
-    }
-
-    while (((len = recv(socket_desc, buffer, BUFSIZ, 0)) > 0)&& (aba > 0)) {
-        fwrite(buffer, sizeof (char), len, received_file);
-        aba -= len;
-        fprintf(stdout, "Recebidos %d bytes e aguardamos :- %d bytes\n", len, aba);
-        if (aba <= 0) {
-            break;
-        }
-    }
-    fclose(received_file);
-    close(socket_desc);
-
-    printf("Cliente finalizado com sucesso!\n");
-    return 0;
+	if((recvbytes = recv(sockfd, buf,MAXDATASIZE,0)) == -1)
+	{
+		perror("recv");
+		close(sockfd);
+		exit(1);
+	}
+	printf("Client receive bytes: %d\n",recvbytes);
+	close(sockfd);
 }
